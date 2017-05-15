cd8b0f8773c4f71ab59d8ada167382413fbfb9a5
Daniil Bladyko <daniil.bladyko@ezpada.com>
2017-05-15 16:47:17 +0200

    Redesign of random testing

--- Spread-option-random-pricing.R
+++ Spread-option-random-pricing.R
@@ -1,4 +1,4 @@
-rm(list=ls())
+#rm(list=ls())
 source('Spread-option-pricing-functions.R')
 ## FIXED PARAMS ##
 # time to expiry
@@ -13,25 +13,36 @@ alpha_2 = 1
 # interest rate
 r = 0.1
 # Monte carlo params
-n_sim = 10^6
+n_sim = 1e6
 sim_timesteps = 2000 # irrelevant in case of MonteCarloGBM3 or MonteCarloGBM4
 # model for joint characteristic function (GBM, SV etc.)
 modelType = modelNames$GBM
-## END ~~ FIXED PARAMS ~~ END ##
+# random variables params
 set.seed(42);
+strikeSign = 0 # negative, positive or zero?
+optType = 'Put' #sample(c("Call","Put"),1) # Call or put
+rOptNum = 10 # number of random options
+# underlyings, volatility and correlations
+underlyings = list()
+# underlyings templates (for SV and GBM)
+underlyings[[1]] <- create.underlyingAsset(S_0 = 100,
+  sigma = NA, delta = 0.05)
+underlyings[[2]] <- create.underlyingAsset(S_0 = NA,
+  sigma = NA, delta = 0.05)
+## END ~~ FIXED PARAMS ~~ END ##
 
-rOptNum = 1
+# random variables
+S_2_0s = getInitPrices(rOptNum);
+sigma_1s = getSigmas(rOptNum);
+sigma_2s = getSigmas(rOptNum);
+Ks = strikeSign * getPosStrikes(n = rOptNum,basis = S_2_0s)
 SpreadOptionPrices = data.frame(FFT = seq_len(rOptNum)*NA,
   MC = seq_len(rOptNum)*NA)
 timeConsum = SpreadOptionPrices
 for (j in 1:rOptNum) {
-  underlyings = list()
-  # underlyings templates (for SV and GBM)
-  underlyings[[1]] <- create.underlyingAsset(S_0 = 100,
-    sigma = runif(1,0.01,0.9), delta = 0.05)
-  underlyings[[2]] <- create.underlyingAsset(S_0 = runif(1,60,140),
-    sigma = runif(1,0.01,0.9), delta = 0.05)
-
+  underlyings[[1]]$setSigma(sigma_1s[j]);
+  underlyings[[2]]$setInitPrice(S_2_0s[j]);
+  underlyings[[2]]$setSigma(sigma_2s[j]);
   # Filler condition sigma
   volatility <- create.stochasticVolatility(nu_0 = 0.04,
     sigma = runif(1,0.01,floor(sqrt(2*1.0*0.04)*100)/100), kappa = 1.0, mu = 0.04)
@@ -41,9 +52,7 @@ for (j in 1:rOptNum) {
   corrs$setCorrelation(underlyings[[1]], volatility, runif(1,-0.9,0.9))
   corrs$setCorrelation(underlyings[[2]], volatility, runif(1,-0.9,0.9))
 
-  K = runif(1,-2,2) * underlyings[[2]]$getInitPrice()
-
-  optType = sample(c("call","put"),1)
+  K = Ks[j]
 
   print(underlyings[[1]]$getParams())
   print(underlyings[[2]]$getParams())
@@ -51,6 +60,7 @@ for (j in 1:rOptNum) {
   print(corrs$getCorrelationMatrix())
   cat("K =",K)
   cat("\nType =",optType)
+  cat("\n")
 
   charFunction = getCharFunction(modelType, initConds = FALSE)
   underlying1 = underlyings[[1]]
@@ -64,7 +74,7 @@ for (j in 1:rOptNum) {
 
   t1 = proc.time()[3]
 
-  SpreadOptionPrices$FFT[j] = getSpreadOptionPricesHurdZhou(K = K, r = r, T_t = T_t)
+  SpreadOptionPrices$FFT[j] = getSpreadOptionPricesHurdZhou(K = K, r = r, T_t = T_t)[,optType]
 
   t2 = proc.time()[3]
 
@@ -86,8 +96,11 @@ for (j in 1:rOptNum) {
   }
 
   SpreadOptionPrices$MC[j] = optionPrices(S_T=mc_sprds,K=K,r=r,T_t=T_t,
-    call=(optType == "call"))
+    call=(optType == "Call"))$prices
 
   t2 = proc.time()[3]
   timeConsum$MC[j] = t2-t1
 }
+
+cat("## TIME ##")
+sapply(timeConsum,mean)
